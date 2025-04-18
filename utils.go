04876f892969eba26acea886d20a78aa3f34d73c
Orlando <espadachinkinno@gmail.com>
2025-04-18 16:07:53 -0600

    Initialize agenda with default contacts in main function

--- utils.go
+++ utils.go
@@ -13,3 +13,28 @@ func LeerEntrada(mensaje string) string {
 	text, _ := reader.ReadString('\n')
 	return strings.TrimSpace(text)
 }
+
+func ContactosIniciales() []Contacto {
+	return []Contacto{
+		{
+			nombre:   "Orlando",
+			telefono: "1234-1234",
+			email:    "orlando@unext.com",
+		},
+		{
+			nombre:   "Miguel",
+			telefono: "4321-1234",
+			email:    "miguel@unext.com",
+		},
+		{
+			nombre:   "Rapha",
+			telefono: "4321-1444",
+			email:    "rapha@unext.com",
+		},
+		{
+			nombre:   "Jorge",
+			telefono: "4365-1234",
+			email:    "jorge@unext.com",
+		},
+	}
+}
